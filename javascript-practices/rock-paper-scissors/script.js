475905c0a1b4e34fbc74b275365c076ae1f90564
Debasish Sahoo <100151159+DebasishSahoo10@users.noreply.github.com>
2022-12-10 22:35:25 +0530

    script : added another logic
    commented the logic

--- javascript-practices/rock-paper-scissors/script.js
+++ javascript-practices/rock-paper-scissors/script.js
@@ -40,3 +40,43 @@ userBtns.forEach(e=>{
         compareChoices(e.value)
     })
 })
+
+
+
+
+
+// ------- ANOTHER LOGIC ------- \\
+
+
+// /* get user's input */
+// let userChoice = prompt('please choose one from rock, paper or scissors, and type into the box below');
+
+// /* convert user's input into index */
+// userChoice = userChoice.toLowerCase();
+// let choiceStack = ['paper', 'rock', 'scissors'];
+// let userChoiceIndex = choiceStack.indexOf(userChoice);
+
+// /* generate computer's choice */
+// let randomNum = Math.floor(Math.random() * 3);
+// let computerChoice = choiceStack[randomNum];
+
+// /* log the each choices on the console */
+// console.log(`Your choice is ${userChoice}, the computer's choice is ${computerChoice}.`);
+
+// /* make a two dimensional array */
+// const results = [
+// ['t', 'c', 'u'],
+// ['u', 't', 'c'],
+// ['c', 'u', 't'],
+// ];
+
+// /* use userChoice index (because in the end we just need the user result) to find the result in this two dimensional array */
+// let userResult = results[randomNum][userChoiceIndex];
+
+// /* make an object to convert the final result into a sentence, so we can log it on the console */
+// const resultMap = {
+// 't': "Tie",
+// 'u': "You win",
+// 'c': "You lose"
+// };
+// console.log(resultMap[userResult]);
