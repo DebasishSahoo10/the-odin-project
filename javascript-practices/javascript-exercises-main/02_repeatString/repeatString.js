0ead0ceab687f1c69fd3046f51db3977c2b44def
Debasish Sahoo <100151159+DebasishSahoo10@users.noreply.github.com>
2022-12-15 11:59:16 +0530

    test : tested multiple cases

--- javascript-practices/javascript-exercises-main/02_repeatString/repeatString.js
+++ javascript-practices/javascript-exercises-main/02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string, n) {
+    if (n<0) {
+        return 'ERROR'
+    } else if (n===0) {
+        return ''
+    } else {
+        let conncattedString = string;
+        for (let i=0; i<(n-1); i++) {
+            conncattedString = conncattedString + string
+        }
+        return conncattedString
+    }
+    
 };
 
 // Do not edit below this line
