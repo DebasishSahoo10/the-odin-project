8832def7d0c73a7e65ec8ff64ebce1da3016f6b3
Debasish Sahoo <100151159+DebasishSahoo10@users.noreply.github.com>
2022-12-15 13:13:22 +0530

    test : tested multiple cases

--- javascript-practices/javascript-exercises-main/05_sumAll/sumAll.js
+++ javascript-practices/javascript-exercises-main/05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(n, m) {
+    let sum = 0;
+    if (typeof n!='number' | typeof m!='number') {
+        return 'ERROR'
+    } else if (n<0 | m<0) {
+        return 'ERROR'
+    } else if (n>m) {
+        for (let i=m ; i<=n; i++) {
+            sum = sum + i
+        }
+    } else {
+        for (let i=n ; i<=m; i++) {
+            sum = sum + i
+        }
+    }
+    return sum
 };
 
 // Do not edit below this line
