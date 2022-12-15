62de545d07465b7fcaa7f7e70083bef392c3e51f
Debasish Sahoo <100151159+DebasishSahoo10@users.noreply.github.com>
2022-12-15 13:44:41 +0530

    test : tested multiple cases

--- javascript-practices/javascript-exercises-main/07_tempConversion/tempConversion.js
+++ javascript-practices/javascript-exercises-main/07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(n) {
+  let num = (n-32) * 0.5556
+  return Math.round(num*10) / 10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(n) {
+  let num = (n*1.8) + 32
+  return Math.round(num*10) / 10
 };
 
 // Do not edit below this line
