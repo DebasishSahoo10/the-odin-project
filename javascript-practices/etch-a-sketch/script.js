aeb5642aec36ffd4de93e860af7734cacf15f8ae
Debasish Sahoo <100151159+DebasishSahoo10@users.noreply.github.com>
2022-12-22 21:28:39 +0530

    feat : added grid creating function
    added grid creating function and event listeners

--- javascript-practices/etch-a-sketch/script.js
+++ javascript-practices/etch-a-sketch/script.js
@@ -3,10 +3,20 @@ const gridTbl = document.querySelector('#grid-tbl')
 
 
 
-
-
 const creatGrid = () => {
-    console.log("on load test")
+    for(let i=0; i<17; i++) {
+        let tableRows = document.createElement("tr")
+        tableRows.id = "row" + i
+        gridTbl.appendChild(tableRows)
+        let rows = document.getElementById("row" + i)
+        for(let j=0; j<17; j++) {
+            let tableColumns = document.createElement("td")
+            rows.appendChild(tableColumns)
+            tableColumns.addEventListener("mouseover", ()=> {
+                tableColumns.style.backgroundColor = "black"
+            })
+        }
+    }
 }
 
 
