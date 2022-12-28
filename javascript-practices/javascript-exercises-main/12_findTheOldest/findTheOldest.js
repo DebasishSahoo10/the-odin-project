ce82d332fd816ddd7eb27d8f484194e5bf537a33
Debasish Sahoo <100151159+DebasishSahoo10@users.noreply.github.com>
2022-12-29 00:25:01 +0530

    added solution

--- javascript-practices/javascript-exercises-main/12_findTheOldest/findTheOldest.js
+++ javascript-practices/javascript-exercises-main/12_findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(obj) {
+    let oldestAge = 0
+    for (i=0; i<obj.length; i++) {
+        if (obj[i].yearOfDeath) {
+            let age = obj[i].yearOfDeath - obj[i].yearOfBirth
+            obj[i].age = age
+        } else {
+            const today = new Date()
+            const currYear = today.getFullYear()
+            let age = currYear - obj[i].yearOfBirth
+            obj[i].age = age
+        }
+    }
 
+    for (i=0; i<obj.length; i++) {
+        if (obj[i].age > oldestAge) {
+            oldestAge = obj[i].age
+        }
+    }
+
+    const resultArr = obj.filter(e => e.age == oldestAge)
+    const resultObj = Object.assign({},...resultArr)
+    return resultObj
 };
 
 // Do not edit below this line
