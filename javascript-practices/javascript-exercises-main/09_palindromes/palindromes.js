c732476eb0a0052d9597a69a95027f00ad75550d
Debasish Sahoo <100151159+DebasishSahoo10@users.noreply.github.com>
2022-12-28 22:17:38 +0530

    added required solution

--- javascript-practices/javascript-exercises-main/09_palindromes/palindromes.js
+++ javascript-practices/javascript-exercises-main/09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let punctionLess = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")
+    let finalString = punctionLess.replaceAll(' ','')
+    let array = finalString.split('').reverse().join('')
+    if (finalString.toLowerCase() == array.toLowerCase()) {
+        return true
+    } else {
+        return false
+    }
 };
 
 // Do not edit below this line
