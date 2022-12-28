9cb5513069ad18b73913dea4ca865cd07dc6fe7e
Debasish Sahoo <100151159+DebasishSahoo10@users.noreply.github.com>
2022-12-28 21:42:09 +0530

    added all solutions

--- javascript-practices/javascript-exercises-main/08_calculator/calculator.js
+++ javascript-practices/javascript-exercises-main/08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(a,b) {
+	const sum = Number(a) + Number(b)
+  return sum
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	const subtraction = Number(a - b)
+  return subtraction
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0
+	for (let i=0; i<array.length; i++) {
+    sum = sum + array[i]
+  }
+  return sum
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let multi = 1
+  for (let i=0; i<array.length; i++) {
+    multi = multi * array[i]
+  }
+  return multi
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	const power = Math.pow(a,b)
+  return power
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let fact = 1
+  for (let i=1; i<=num; i++) {
+    fact = fact * i
+  }
+  return fact
 };
 
 // Do not edit below this line
