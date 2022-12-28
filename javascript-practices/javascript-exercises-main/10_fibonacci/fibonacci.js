8dfdabc4178a4e132eef1dcaf3e6c213e448db04
Debasish Sahoo <100151159+DebasishSahoo10@users.noreply.github.com>
2022-12-28 22:42:21 +0530

    added solution

--- javascript-practices/javascript-exercises-main/10_fibonacci/fibonacci.js
+++ javascript-practices/javascript-exercises-main/10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    let num = Number(a)
+    if (num < 0) return "OOPS"
+    let num1=0;
+    let num2=1;
+    let sum;
+    for (let i = 1; i < num; i++) 
+    {
+        sum=num1+num2;
+        num1=num2;
+        num2=sum;
+    }
+    return num2;
 };
 
 // Do not edit below this line
