c47fa86156a9a223733dd13a1febd4b361e8264e
Debasish Sahoo <100151159+DebasishSahoo10@users.noreply.github.com>
2023-01-04 23:57:20 +0530

    feat : added details keeping screen

--- javascript-practices/calculator/script.js
+++ javascript-practices/calculator/script.js
@@ -2,13 +2,16 @@ const numBtns = document.querySelectorAll('.num-btns')
 const equalBtn = document.querySelector('#equal-btn')
 const mainDisplay = document.querySelector('#large-display')
 const operatorBtns = document.querySelectorAll('.operator-btns')
-
+const upperDisplay = document.querySelector('#upper-display')
 
 
 let firstNum = ''
 let secondNum = ''
 let choosedSign = ''
 let result = ''
+let upperContent = ''
+
+
 
 const operatorsFunc = (num1, num2, operator) => {
     if (operator == '+') {
@@ -27,9 +30,12 @@ numBtns.forEach((e)=>{
        if (choosedSign) {
         secondNum = secondNum + e.target.innerText
         mainDisplay.innerText = secondNum
+        upperContent =  `${upperContent} ${secondNum}`
+        upperDisplay.innerText = upperContent
        } else {
         firstNum = firstNum + e.target.innerText
         mainDisplay.innerText = firstNum
+        upperContent =  `${upperContent} ${firstNum}`
        }
     })
 })
@@ -38,15 +44,18 @@ operatorBtns.forEach((e)=>{
     e.addEventListener("click", (e)=>{
         if (secondNum) {
             operatorsFunc(firstNum, secondNum, choosedSign)
-            mainDisplay.innerText = result
+            choosedSign = e.target.innerText
             firstNum = result
             secondNum = ''
-            choosedSign = e.target.innerText
+            upperContent = `${result} ${choosedSign}`
+            upperDisplay.innerText = upperContent
         } else if (result) {
             firstNum = result
             choosedSign = e.target.innerText
         } else {
             choosedSign = e.target.innerText
+            upperContent = `${upperContent} ${choosedSign}`
+            upperDisplay.innerText = upperContent
         }
     })
 })
