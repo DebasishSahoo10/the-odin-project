a78a5ca05b0c380bc995eb93423f2160db83540e
Debasish Sahoo <100151159+DebasishSahoo10@users.noreply.github.com>
2023-01-04 22:37:27 +0530

    feat : added some basic feats

---
+++ javascript-practices/calculator/script.js
@@ -0,0 +1,53 @@
+const numBtns = document.querySelectorAll('.num-btns')
+const equalBtn = document.querySelector('#equal-btn')
+const mainDisplay = document.querySelector('#large-display')
+const operatorBtns = document.querySelectorAll('.operator-btns')
+
+
+
+let firstNum = ''
+let secondNum = ''
+// let testNum = ''
+let choosedSign = ''
+let result = ''
+
+const operatorsFunc = (num1, num2, operator) => {
+    if (operator == '+') {
+        result = Number(num1) + Number(num2)
+    } else if (operator == '-') {
+        result = Number(num1) - Number(num2)
+    } else if (operator == 'x') {
+        result = Number(num1) * Number(num2)
+    } else if (operator == '%') {
+        result = Number(num1) / Number(num2)
+    }
+}
+
+numBtns.forEach((e)=>{
+    e.addEventListener("click", (e)=>{
+       if (choosedSign) {
+        secondNum = secondNum + e.target.innerText
+        mainDisplay.innerText = secondNum
+       } else {
+        firstNum = firstNum + e.target.innerText
+        mainDisplay.innerText = firstNum
+       }
+    })
+})
+
+
+equalBtn.addEventListener("click", ()=>{
+    // console.log(firstNum)
+    // console.log(secondNum)
+    operatorsFunc(firstNum, secondNum, choosedSign)
+    firstNum = ''
+    secondNum = ''
+    choosedSign = ''
+    console.log(result)
+})
+
+operatorBtns.forEach((e)=>{
+    e.addEventListener("click", (e)=>{
+        choosedSign = e.target.innerText
+    })
+})
