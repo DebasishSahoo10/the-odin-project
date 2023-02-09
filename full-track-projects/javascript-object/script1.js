40c03cf2c73583bd518cc3639a7866638e42a2c5
Debasish Sahoo <100151159+DebasishSahoo10@users.noreply.github.com>
2023-02-09 13:29:54 +0530

    created object constructor

--- full-track-projects/javascript-object/script1.js
+++ full-track-projects/javascript-object/script1.js
@@ -1,1 +1,12 @@
-console.log("testing")
+function book (tittle, author, pages, readStat) {
+    this.tittle = tittle
+    this.author = author
+    this.pages = pages
+    this.readStat = readStat
+    this.info = function () {
+        return this.tittle + " by " + this.author + "," + this.pages + "," + this.readStat
+    }
+}
+
+const player1 = new book("The Hobbit","J.R.R. Tolkien","295 pages","not read yet")
+console.log(player1.info())
