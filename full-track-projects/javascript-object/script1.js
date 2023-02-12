601dc4e6186b394b296f6933fb59e74c73e16b23
Debasish Sahoo <100151159+DebasishSahoo10@users.noreply.github.com>
2023-02-12 22:15:01 +0530

    feat : added new book addition

--- full-track-projects/javascript-object/script1.js
+++ full-track-projects/javascript-object/script1.js
@@ -1,9 +1,4 @@
-// function book (tittle, author, pages, readStat) {
-//     this.tittle = tittle
-//     this.author = author
-//     this.pages = pages
-//     this.readStat = readStat
-// }
+
 
 // book.prototype.info = function () {
 //     return this.tittle + " by " + this.author + "," + this.pages + "," + this.readStat
@@ -12,19 +7,35 @@
 // const player1 = new book("The Hobbit","J.R.R. Tolkien","295 pages","not read yet")
 // console.log(player1.info())
 
+const myLibrary = []
+
 const addBtnCard = document.getElementById('add-btn-card')
 const cardContainer = document.getElementById('card-container')
 const newBookBtn = document.getElementById('newBookBtn')
 const newBookForm = document.getElementById('newBookForm')
 const formCloseBtn = document.getElementById('formCloseBtn')
+const tittleInput = document.getElementById('tittle-input')
+const authorInput = document.getElementById('author-input')
+const pagesInput = document.getElementById('pages-input')
+const readStatus = document.getElementById('read-status')
 
-
+function book (tittle, author, pages, readStat) {
+    this.tittle = tittle
+    this.author = author
+    this.pages = pages
+    this.readStat = readStat
+}
 
 addBtnCard.addEventListener("click", ()=>{
     newBookForm.style.display = "block"
 })
 formCloseBtn.addEventListener("click", ()=>{
     newBookForm.style.display = "none"
+})
+newBookBtn.addEventListener("click", (event)=>{
+    event.preventDefault()
+    const newBook = new book(tittleInput.value, authorInput.value, pagesInput.value, readStatus.checked)
+    // console.log(newBook)
 })
 
 // addBtnCard.addEventListener("click", ()=>{
