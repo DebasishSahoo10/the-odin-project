856316167b76ef83ca1cfe19b5d53e0be12a3dda
Debasish Sahoo <100151159+DebasishSahoo10@users.noreply.github.com>
2023-02-12 20:49:12 +0530

    commented existing codes

--- full-track-projects/javascript-object/script1.js
+++ full-track-projects/javascript-object/script1.js
@@ -1,13 +1,13 @@
-function book (tittle, author, pages, readStat) {
-    this.tittle = tittle
-    this.author = author
-    this.pages = pages
-    this.readStat = readStat
-}
+// function book (tittle, author, pages, readStat) {
+//     this.tittle = tittle
+//     this.author = author
+//     this.pages = pages
+//     this.readStat = readStat
+// }
 
-book.prototype.info = function () {
-    return this.tittle + " by " + this.author + "," + this.pages + "," + this.readStat
-}
+// book.prototype.info = function () {
+//     return this.tittle + " by " + this.author + "," + this.pages + "," + this.readStat
+// }
 
-const player1 = new book("The Hobbit","J.R.R. Tolkien","295 pages","not read yet")
-console.log(player1.info())
+// const player1 = new book("The Hobbit","J.R.R. Tolkien","295 pages","not read yet")
+// console.log(player1.info())
