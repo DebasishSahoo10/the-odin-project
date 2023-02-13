22a18c1cdaadfe89a023bb0128bcafcfb4762600
Debasish Sahoo <100151159+DebasishSahoo10@users.noreply.github.com>
2023-02-13 13:59:22 +0530

    source : added prevously made files

--- full-track-projects/javascript-object/react-version/src/App.jsx
+++ full-track-projects/javascript-object/react-version/src/App.jsx
@@ -2,11 +2,27 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <div className="App">
       
+      <div id="card-container">
+        <div class="card" id="add-btn-card">+</div>
+      </div>
+    
+      <form action="" id="newBookForm">
+        <label for="tittle-input">Book Name : </label>
+        <input type="text" id="tittle-input"></input>
+        <label for="author-input">Author : </label>
+        <input type="text" id="author-input"></input>
+        <label for="pages">No. Of Pages : </label>
+        <input type="number" id="pages-input"></input>
+        <label for="read-status">Read or Not : </label>
+        <input type="checkbox" id="read-status"></input>
+        <button id="newBookBtn">Submit</button>
+        <button id="formCloseBtn">X</button>
+      </form>
       
     </div>
   )
