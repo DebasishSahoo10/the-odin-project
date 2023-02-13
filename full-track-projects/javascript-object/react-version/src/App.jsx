24e2db921b1feb5075f4a1353a8d57b89b262b63
Debasish Sahoo <100151159+DebasishSahoo10@users.noreply.github.com>
2023-02-13 14:59:48 +0530

    feat : added form show/close options

--- full-track-projects/javascript-object/react-version/src/App.jsx
+++ full-track-projects/javascript-object/react-version/src/App.jsx
@@ -2,23 +2,26 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  
+  const [formState, setFormState] = useState()
+  const showForm = () => {
+    setFormState("block")
+  }
 
   return (
     <div className="App">
-      
+
       <div id="card-container">
-        <div class="card" id="add-btn-card">+</div>
+        <div className="card" id="add-btn-card" onClick={showForm}>+</div>
       </div>
     
-      <form action="" id="newBookForm">
-        <label for="tittle-input">Book Name : </label>
+      <form action="" id="newBookForm" className={formState}>
+        <label htmlFor="tittle-input">Book Name : </label>
         <input type="text" id="tittle-input"></input>
-        <label for="author-input">Author : </label>
+        <label htmlFor="author-input">Author : </label>
         <input type="text" id="author-input"></input>
-        <label for="pages">No. Of Pages : </label>
+        <label htmlFor="pages">No. Of Pages : </label>
         <input type="number" id="pages-input"></input>
-        <label for="read-status">Read or Not : </label>
+        <label htmlFor="read-status">Read or Not : </label>
         <input type="checkbox" id="read-status"></input>
         <button id="newBookBtn">Submit</button>
         <button id="formCloseBtn">X</button>
