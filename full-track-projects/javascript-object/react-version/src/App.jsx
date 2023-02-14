0c42c8a4a980a4c092267d8d40ec111fc9ba6d11
Debasish Sahoo <100151159+DebasishSahoo10@users.noreply.github.com>
2023-02-14 20:42:12 +0530

    style and content : form

--- full-track-projects/javascript-object/react-version/src/App.jsx
+++ full-track-projects/javascript-object/react-version/src/App.jsx
@@ -106,19 +106,30 @@ function App() {
       </div>
     
       <form action="" id="newBookForm" className={formState}>
-        <label htmlFor="tittle-input">Book Name : </label>
-        <input type="text" id="tittle-input" onChange={(e)=>setTittle(e.target.value)}></input>
-        <label htmlFor="author-input">Author : </label>
-        <input type="text" id="author-input" onChange={(e)=>setAuthor(e.target.value)}></input>
-        <label htmlFor="pages">No. Of Pages : </label>
-        <input type="number" id="pages-input" onChange={(e)=>setPages(e.target.value)}></input>
-        <label htmlFor="read-status">Read or Not : </label>
-        <input type="checkbox" id="read-status" onChange={(e)=>setReadStat(e.target.checked)}></input>
-        <button id="newBookBtn" type='submit' onClick={addToLibrary}>Submit</button>
-        <button id="formCloseBtn" onClick={(e)=>{
-          e.preventDefault()
-          setFormState("none")
-        }}>X</button>
+        
+          <label htmlFor="tittle-input">Book Name : </label>
+          <input type="text" id="tittle-input" onChange={(e)=>setTittle(e.target.value)}></input>
+        
+        
+          <label htmlFor="author-input">Author : </label>
+          <input type="text" id="author-input" onChange={(e)=>setAuthor(e.target.value)}></input>
+        
+        
+          <label htmlFor="pages">No. Of Pages : </label>
+          <input type="number" id="pages-input" onChange={(e)=>setPages(e.target.value)}></input>
+        
+        
+          <label htmlFor="read-status">Read or Not : </label>
+          <input type="checkbox" id="read-status" onChange={(e)=>setReadStat(e.target.checked)}></input>
+        
+        <div>
+          <button id="newBookBtn" type='submit' onClick={addToLibrary} className='submit-btn'>Submit</button>
+          <button id="formCloseBtn" onClick={(e)=>{
+            e.preventDefault()
+            setFormState("none")
+          }} className='close-btn'>close</button>
+        </div>
+        
       </form>
       
     </div>
